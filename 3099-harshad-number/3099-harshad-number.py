48abc924dc63f34461c62ff1c3d8cad68c52017f
sakthivel232008-pixel <sakthivel232008@gmail.com>
2026-09-25 11:47:04 +0700

    Time: 0 ms (100%), Space: 19.3 MB (21.81%) - LeetHub

---
+++ 3099-harshad-number/3099-harshad-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sumOfTheDigitsOfHarshadNumber(self, x: int) -> int:
+        s = str(x)
+        t = 0
+        for i  in s :
+            t += int(i)
+        if (x % t  == 0 ):
+            return t 
+        else :
+            return -1
+        
